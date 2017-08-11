db124b3f3fad8287632a586e7a1c8ef8c83a26a7
Donna Ann Issac <donna.ann1209@gmail.com>
2017-08-11 07:40:34 +0530

    Added object parser

--- jsonParser.js
+++ jsonParser.js
@@ -7,23 +7,26 @@ fs.readFile('sample.json', 'utf-8', function(err, inpStr) {
 function nullParser(data) {
   if (data.substr(0,4) === "null")
     var resData = data.slice(4)
-    var resultArray = []
-    resultArray.push(null, resData)
-    return(resultArray)
+    //var resultArray = []
+    //resultArray.push(null, resData)
+    //console.log(resultArray[0]);
+    return([null, resData])
 }
 
 function boolParser(data) {
   if(data.substr(0,4) === "true") {
     var resData = data.slice(4)
-    var resultArray = []
-    resultArray.push(true, resData)
-    return(resultArray)
+    //var resultArray = []
+    //resultArray.push(true, resData)
+    //console.log(resultArray);
+    return([true, resData])
   }
   else if(data.substr(0,5) === "false") {
     var resData = data.slice(5)
-    var resultArray = []
-    resultArray.push(false, resData)
-    return(resultArray)
+    //var resultArray = []
+    //resultArray.push(false, resData)
+    //console.log(resultArray);
+    return([false, resData])
   }
 }
 
@@ -36,15 +39,17 @@ function commaParser(data) {
 
 function colonParser(data) {
   var resData = data.slice(1)
-  console.log(":", resData)
+  return resData
 }
 
 function numParser(data) {
   var parsedNumString = data.match(/-?\d+\.?\d*/).toString()
-  var resData = data.slice(parsedNumString.length)
-  var resultArray = []
-  resultArray.push(parsedNumString, resData)
-  return(resultArray)
+  var index = data.indexOf(",")
+  var resData = data.slice(index)
+  //var resultArray = []
+  //resultArray.push(parsedNumString, resData)
+  //console.log("In num",resultArray)
+  return([parsedNumString, resData])
 
 }
 
@@ -52,57 +57,80 @@ function stringParser(data) {
   var parsedString = data.match(/[a-zA-Z]+/).toString()
   var i = data.slice(1).search(/"/)
   var resData = data.slice(i+2)
-  var resultArray = []
-  resultArray.push(parsedString, resData)
-  return(resultArray)
+  //var resultArray = []
+  //resultArray.push(parsedString, resData)
+  //console.log(resultArray);
+  return([parsedString, resData])
 }
 
 function arrayParser(data) {
-  parsedArray = []
-  var arrayElement, remainingData
+  var parsedArray = []
   while(data.charAt(0) != ']') {
-    [arrayElement, remainingData] = valueParser(data)
-    parsedArray.push(arrayElement)
-    data = commaParser(remainingData)
+    console.log("hey", data.charAt(0));
+    var result = valueParser(data)
+    console.log("In Arr after val", result)
+    parsedArray.push(result[0])
+    console.log(parsedArray);
+    data = result[1]
+    if(data.startsWith(','))
+    data = commaParser(result[1])
+    console.log(data);
   }
-  console.log(parsedArray)
-  //return ([parsedArray, data.slice(1)])
+  //console.log(parsedArray)
+  return ([parsedArray, data.slice(1)])
 }
 
 function valueParser(data) {
-  var value, remainingData, resultArray = []
-  if(data.charAt(0) == '"' ) {
-    [value, remainingData] = stringParser(data.slice(1))
-    resultArray.push(value, remainingData)
-    return (resultArray)
+  var resultArray = []
+  if(data.charAt(0) == '[') {
+    console.log("Before array in val", data.slice(1));
+    var parsedArray = arrayParser(data.slice(1))
+    console.log("After array in val", parsedArray);
+    return parsedArray
   }
-  else if(data.charAt(0) == /\d/) {
-    [value, remainingData] = numParser(data)
-    resultArray.push(value, remainingData)
-    return (resultArray)
+  else if(data.charAt(0) == '"' ) {
+    resultArray = stringParser(data.slice(1))
+    return resultArray
   }
-  else if(data.charAt(0) == '[') {
-      arrayParser(data.slice(1))
-    }
-  else if(data.charAt(0) == /true|false/) {
-    [value, remainingData] = boolParser(data)
-    resultArray.push(value, remainingData)
-    return (resultArray)
+  else if(/[0-9]/.test(data.charAt(0))) {
+    resultArray = numParser(data)
+    console.log("During array after num",resultArray);
+    return resultArray
   }
-  else if(data.charAt(0) == null) {
-    [value, remainingData] = nullParser(data)
-    resultArray.push(value, remainingData)
-    return (resultArray)
+  else if(/t|f/.test(data.charAt(0))) {
+    resultArray = boolParser(data)
+    return resultArray
+  }
+  else if(data.substr(0,4) == 'null') {
+    resultArray = nullParser(data)
+    return resultArray
   }
   else if(data.charAt(0) == '{') {
+    console.log("hey");
     objectParser(data.slice(1))
   }
 }
 
-//function objectParser(data) {
+function objectParser(data) {
+  var property, value, parsedObject = {}
+  while(data.charAt(0 != '}')) {
+    var temp = valueParser(data)
 
-//}
-const readline = require('readline')
+    property = temp[0]
+    data = colonParser(temp[1])
+    temp = valueParser(data)
+    value = temp[0]
+    data = temp[1]
+    if(data.startsWith(','))
+    data = commaParser(temp[1])
+    parsedObject[property] = value
+    console.log(parsedObject);
+  }
+  console.log(parsedObject);
+  return parsedObject
+}
+
+const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin
 })
