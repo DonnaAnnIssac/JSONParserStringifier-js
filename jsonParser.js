5c193918c264f4be4031d4d6dd8d31c116eb5cd6
Donna Ann Issac <donna.ann1209@gmail.com>
2017-08-24 17:33:13 +0530

    updated function signatures of all individual parsers to be the same

--- jsonParser.js
+++ jsonParser.js
@@ -1,33 +1,50 @@
 function nullParser(data) {
-  var resData = data.slice(4)
-  return([null, resData])
+  if(data.substr(0,4) == 'null') {
+    var resData = data.slice(4)
+    console.log("In null parser");
+    return([null, resData])
+  }
+  else {
+    return null
+  }
 }
 
 function boolParser(data) {
+
   if(data.substr(0,4) === "true") {
     var resData = data.slice(4)
+    console.log("In bool Parser");
     return([true, resData])
   }
   else if(data.substr(0,5) === "false") {
     var resData = data.slice(5)
+    console.log("In bool Parser");
     return([false, resData])
+  }
+  else {
+    return null
   }
 }
 
 function commaParser(data) {
   if(data.startsWith(',')) {
     var resData = data.slice(1)
-    return resData
+    console.log("In comma parser");
+    return ([',', resData])
   }
   else {
-    return data
+    return null
   }
 }
 
 function colonParser(data) {
   if(data.startsWith(':')) {
     var resData = data.slice(1)
-    return resData
+    console.log("In colon parser");
+    return ([':', resData])
+  }
+  else {
+    return null
   }
 }
 
@@ -38,26 +55,40 @@ function spaceParser(data) {
 }
 
 function numParser(data) {
-  //console.log(data)
-  var parsedNum = (/[+-]?\d+\.?[eE]?[+-]?\d*/).exec(data).toString()
+  var parsedNum = (/[+-]?\d*\.?\d*([eE][+-]?\d*)?/).exec(data).toString()
   //console.log(parsedNum);
-  if(parsedNum != null) {
+  if(parsedNum) {
     var resData = data.slice(parsedNum.length)
+    console.log("In number parser");
     return([parsedNum, resData])
+  }
+  else {
+    return null
   }
 }
 
 function stringParser(data) {
-  var parsedString = data.match(/[a-zA-Z0-9\:\/\.\-\\]+/).toString()
-  var i = data.slice(1).search(/"/)
-  var resData = data.slice(i+2)
+  if (data[0] != '"') {
+    return null
+  }
+  //var parsedString = data.match(/[a-zA-Z0-9\:\/\.\-\\]+/).toString()
+  //var i = data.slice(1).search(/"/)
+  //var resData = data.slice(i+2)
   //console.log(parsedString);
+  var temp = data.slice(1)
+  var i = temp.indexOf('"')
+  var parsedString = data.slice(0,i+2)
+  var resData = data.slice(i+3)
+  console.log("In string parser");
   return([parsedString, resData])
 }
 
 function arrayParser(data) {
+  if (data[0] != '[') {
+    return null
+  }
+  console.log("In array parser");
   var parsedArray = []
-  //if()
   while(data.charAt(0) != ']') {
     var result = valueParser(data)
     parsedArray.push(result[0])
@@ -69,10 +100,13 @@ function arrayParser(data) {
 }
 
 function objectParser(data) {
+  if (data[0] != '{') {
+    return null
+  }
+  console.log("In Object parser");
   var property, value, parsedObject = {}
   while(data.charAt(0) != '}') {
     var temp = valueParser(data)
-    //console.log("here",temp)
     property = temp[0]
     data = colonParser(temp[1])
     temp = valueParser(data)
@@ -86,48 +120,26 @@ function objectParser(data) {
 }
 
 function valueParser(data) {
-  var resultArray = []
-  if(data.charAt(0) == '{') {
-    var parsedObject = objectParser(data.slice(1))
-    //console.log("after objparser");
-    return parsedObject
-  }
-  else if(data.charAt(0) == '[') {
-    var parsedArray = arrayParser(data.slice(1))
-    return parsedArray
-  }
-  else if(data.charAt(0) == '"' ) {
-    resultArray = stringParser(data.slice(1))
-    return resultArray
-  }
-  else if(/[0-9\.+-]/.test(data.charAt(0))) {
-    resultArray = numParser(data)
-    return resultArray
-  }
-  else if(/t|f/.test(data.charAt(0))) {
-    resultArray = boolParser(data)
-    return resultArray
-  }
-  else if(data.substr(0,4) == 'null') {
-    resultArray = nullParser(data)
-    return resultArray
-  }
-
+  console.log("In value Parser")
+  if(nullParser(data) != null)
+    return nullParser(data)
+  else if(boolParser(data) != null)
+    return boolParser(data)
+  else if(numParser(data) != null)
+    return numParser(data)
+  else if(stringParser(data) != null)
+    return stringParser(data)
+  else if(arrayParser(data) != null)
+    return arrayParser(data)
+  else if(objectParser(data) != null)
+    return objectParser(data)
 }
 
 const filename = process.argv[2]
 const fs = require('fs')
 fs.readFile(filename, 'utf-8', function(err, inpStr) {
             if(err) throw err
-            var data = spaceParser(inpStr)
-            var opObj = valueParser(data)
+            //var data = spaceParser(inpStr)
+            var opObj = valueParser(inpStr)
             console.log(opObj[0])
           })
-/*const readline = require('readline');
-          const rl = readline.createInterface({
-            input: process.stdin
-          })
-
-          rl.on('line', (line) => {
-                      valueParser(line)
-                      rl.close()})*/
