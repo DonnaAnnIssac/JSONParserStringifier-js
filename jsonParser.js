7bab8bb7f1a8a995858f080d0b3d34c3581322d4
Donna Ann Issac <donna.ann1209@gmail.com>
2017-08-28 11:04:22 +0530

    Modified spaceParser to return null if no whitespace found

--- jsonParser.js
+++ jsonParser.js
@@ -8,30 +8,29 @@ fs.readFile(filename, 'utf-8', function(err, inpStr) {
 
 function nullParser(data) {
   if(data.substr(0,4) == 'null') {
-    let resData = data.slice(4)
-    resData = spaceParser(resData)
-    return([null, resData])
+    let resData = data.slice(4), spaceParsedData = null
+    if((spaceParsedData = spaceParser(resData)) != null)
+      return([null, spaceParsedData[1]])
+    else return ([null, resData])
   }
-  else {
-    return null
-  }
+  return null
 }
 
 function boolParser(data) {
-  let resData = null
+  let resData = null, spaceParsedData = null
   if(data.substr(0,4) === "true") {
     resData = data.slice(4)
-    resData = spaceParser(resData)
-    return([true, resData])
+    if((spaceParsedData = spaceParser(resData)) != null)
+      return([true, spaceParsedData[1]])
+    else return ([true, resData])
   }
   else if(data.substr(0,5) === "false") {
     resData = data.slice(5)
-    resData = spaceParser(resData)
-    return([false, resData])
+    if((spaceParsedData = spaceParser(resData)) != null)
+      return([false, spaceParsedData[1]])
+    else return ([false, resData])
   }
-  else {
-    return null
-  }
+  return null
 }
 
 function commaParser(data) {
@@ -51,47 +50,50 @@ function colonParser(data) {
 function spaceParser(data) {
   if((/^(\s)+/).test(data)) {
     data = data.replace(/^(\s)+/, '')
+    return ([' ', data])
   }
-  return data
+  return null
 }
 
 function numParser(data) {
-  let parsedNum = (/^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/).exec(data)
+  let parsedNum = (/^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/).exec(data), spaceParsedData = null
   if(parsedNum) {
     parsedNum = parsedNum[0]
     let resData = data.slice(parsedNum.length)
-    resData = spaceParser(resData)
     parsedNum = parseInt(parsedNum)
-    return([parsedNum, resData])
+    if((spaceParsedData = spaceParser(resData)) != null)
+        return([parsedNum, spaceParsedData[1]])
+    else return ([parsedNum, resData])
   }
-  else {
-    return null
-  }
+  return null
 }
 
 function stringParser(data) {
   if (data[0] != '"') {
     return null
   }
-  let i = data.slice(1).indexOf('"')
+  let i = data.slice(1).indexOf('"'), spaceParsedData = null
   let parsedString = data.slice(1,i+1).toString()
   let resData = data.slice(i+2)
-  resData = spaceParser(resData)
-  return([parsedString, resData])
+  if((spaceParsedData = spaceParser(resData)) != null)
+    return ([parsedString, spaceParsedData[1]])
+  else return([parsedString, resData])
 }
 
 function arrayParser(data) {
   if (data[0] != '[') {
     return null
   }
-  let parsedArray = []
+  let parsedArray = [], result = null, spaceParsedData = null
   data = data.slice(1)
-  while(data.charAt(0) != ']') {
-    data = spaceParser(data)
-    let result = valueParser(data)
+  while(data[0] != ']') {
+    if((spaceParsedData = spaceParser(data)) != null)
+      result = valueParser(spaceParsedData[1])
+    else result = valueParser(data)
     parsedArray.push(result[0])
     data = commaParser(result[1])
-    data = spaceParser(data)
+    if((spaceParsedData = spaceParser(data)) != null)
+      data = spaceParsedData[1]
   }
   return ([parsedArray, data.slice(1)])
 }
@@ -100,21 +102,23 @@ function objectParser(data) {
   if (data[0] != '{') {
     return null
   }
-  let property, value, parsedObject = {}
+  let property, value, parsedObject = {}, temp = null, spaceParsedData =  null
   data = data.slice(1)
-  while(data.charAt(0) != '}') {
-    data = spaceParser(data)
-    let temp = valueParser(data)
+  while(data[0] != '}') {
+    if((spaceParsedData = spaceParser(data)) != null)
+      temp = valueParser(spaceParsedData[1])
+    else temp = valueParser(data)
     property = temp[0]
     data = colonParser(temp[1])
-    data = spaceParser(data)
+    if((spaceParsedData = spaceParser(data)) != null)
+      data = spaceParsedData[1]
     temp = valueParser(data)
     value = temp[0]
     data = commaParser(temp[1])
-    data = spaceParser(data)
+    if((spaceParsedData = spaceParser(data)) != null)
+      data = spaceParsedData[1]
     parsedObject[property] = value
   }
-  console.log(parsedObject);
   return ([parsedObject, data.slice(1)])
 }
 
