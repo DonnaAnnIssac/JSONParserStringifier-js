c30ed99fd14dca2978ac8c661800e36f4a14c597
Donna Ann Issac <donna.ann1209@gmail.com>
2017-08-28 14:15:08 +0530

    Added parserFactory that returns the parser that returns a non null value

--- jsonParser.js
+++ jsonParser.js
@@ -6,7 +6,7 @@ fs.readFile(filename, 'utf-8', function(err, inpStr) {
             console.log(opObj[0])
           })
 
-function nullParser(data) {
+const nullParser = function(data) {
   if(data.substr(0,4) == 'null') {
     let resData = data.slice(4), spaceParsedData = null
     if((spaceParsedData = spaceParser(resData)) != null)
@@ -16,7 +16,7 @@ function nullParser(data) {
   return null
 }
 
-function boolParser(data) {
+const boolParser = function(data) {
   let resData = null, spaceParsedData = null
   if(data.substr(0,4) === "true") {
     resData = data.slice(4)
@@ -33,21 +33,21 @@ function boolParser(data) {
   return null
 }
 
-function commaParser(data) {
+const commaParser = function(data) {
   if(data.startsWith(',')) {
     data = data.slice(1)
   }
   return data
 }
 
-function colonParser(data) {
+const colonParser = function(data) {
   if(data.startsWith(':')) {
     data = data.slice(1)
   }
   return data
 }
 
-function spaceParser(data) {
+const spaceParser = function(data) {
   if((/^(\s)+/).test(data)) {
     data = data.replace(/^(\s)+/, '')
     return ([' ', data])
@@ -55,7 +55,7 @@ function spaceParser(data) {
   return null
 }
 
-function numParser(data) {
+const numParser = function(data) {
   let parsedNum = (/^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/).exec(data), spaceParsedData = null
   if(parsedNum) {
     parsedNum = parsedNum[0]
@@ -68,7 +68,7 @@ function numParser(data) {
   return null
 }
 
-function stringParser(data) {
+const stringParser = function(data) {
   if (data[0] != '"') {
     return null
   }
@@ -77,10 +77,10 @@ function stringParser(data) {
   let resData = data.slice(i+2)
   if((spaceParsedData = spaceParser(resData)) != null)
     return ([parsedString, spaceParsedData[1]])
-  else return([parsedString, resData])
+  return([parsedString, resData])
 }
 
-function arrayParser(data) {
+const arrayParser = function(data) {
   if (data[0] != '[') {
     return null
   }
@@ -98,15 +98,16 @@ function arrayParser(data) {
   return ([parsedArray, data.slice(1)])
 }
 
-function objectParser(data) {
+const objectParser = function(data) {
   if (data[0] != '{') {
     return null
   }
   let property, value, parsedObject = {}, temp = null, spaceParsedData =  null
   data = data.slice(1)
   while(data[0] != '}') {
-    if((spaceParsedData = spaceParser(data)) != null)
+    if((spaceParsedData = spaceParser(data)) != null) {
       temp = valueParser(spaceParsedData[1])
+    }
     else temp = valueParser(data)
     property = temp[0]
     data = colonParser(temp[1])
@@ -122,18 +123,17 @@ function objectParser(data) {
   return ([parsedObject, data.slice(1)])
 }
 
+function parserFactory(data) {
+  const parsers = [nullParser, boolParser, numParser, stringParser, arrayParser, objectParser]
+  let result = parsers.filter(function(parser) {
+                        if(parser(data) != null) {
+                          return parser
+                        }
+                      })
+  return result
+}
 function valueParser(data) {
-  let resultArray = []
-  if((resultArray = nullParser(data)) != null)
-    return resultArray
-  else if((resultArray = boolParser(data)) != null)
-    return resultArray
-  else if((resultArray = numParser(data)) != null)
-    return resultArray
-  else if((resultArray = stringParser(data)) != null)
-    return resultArray
-  else if((resultArray = arrayParser(data)) != null)
-    return resultArray
-  else if((resultArray = objectParser(data)) != null)
-    return resultArray
+  let result = parserFactory(data)
+  let resultArray = result[0](data)
+  return resultArray
 }
